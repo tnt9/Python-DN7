7dff334d99447b901031a1ee989a2d6c14ae4d69
Nina Tratnik <nina.tratnik@gmail.com>
2016-05-02 19:32:56 +0200

    sprememba za stevilke izven intervala

--- Neznana_stevilka.py
+++ Neznana_stevilka.py
@@ -1,26 +1,33 @@
 import random
 
-your_number = int(raw_input("Please enter your number (between 1 and 10):"))
+your_number = int(raw_input("Please enter your number (between 1 and 10): "))
 print("Your number is %i" % your_number)
-
 number = random.randint(1, 10)
+n = 1
 
-if int(your_number) < number:
-    print("Your number is smaller then the random number.")
-    your_number2 = int(raw_input("Guess again: "))
-    if int(your_number2) == number:
-        print("You've guessed correctly the random number!")
+if n < 2:
+    if your_number < 1:
+        print ("Chosen number is not in the range between 1 and 10. It is smaller.")
+        your_number = int(raw_input("Please, choose once again: "))
+    n = n
+    if your_number > 10:
+        print("Chosen number is not in the range between 1 and 10. It is higher.")
+        your_number = int(raw_input("Please, choose once again: "))
+    n = n
+    if your_number < number:
+        print("Your number is smaller then the random number.")
+        your_number2 = int(raw_input("Please, choose again: "))
+        if your_number2 < number:
+            print("Your number is still smaller then the random number.")
+        elif your_number2 > number:
+            print("Your number is bigger then the random number.")
+        else:
+            print ("You've guessed correctly the random number!")
+    elif your_number > number:
+        print("Your number is bigger then the random number.")
+        your_number2 = int(raw_input("Please, choose again: "))
     else:
-        print("Even your second guess is wrong!")
-
-elif your_number > number:
-    print("Your number is bigger then the random number.")
-    your_number2 = int(raw_input("Guess again: "))
-    if your_number2 == number:
         print("You've guessed correctly the random number!")
-    else:
-        print("Even your second guess in wrong!")
-else:
-    print("You've guessed correctly the random number!")
+    n = n + 1
 
 print("The random number is %i" % number)
